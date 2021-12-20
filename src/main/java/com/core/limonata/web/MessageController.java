224d093d522213325cb62987c9f8e07dd07bb744
Emre <emre-gurkan@hotmail.com>
2021-12-20 23:23:24 +0300

    CORS Configuration

--- src/main/java/com/core/limonata/web/MessageController.java
+++ src/main/java/com/core/limonata/web/MessageController.java
@@ -2,12 +2,14 @@ package com.core.limonata.web;
 
 import com.core.limonata.data.models.Message;
 import com.core.limonata.service.MessageService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "message")
 public class MessageController {
